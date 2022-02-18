77b033728420112e0923f0e0bf0c2817a8a8c662
amin <signorprgrmr@gmail.com>
2022-02-18 12:54:58 +0330

    Tamrin haye jalaseye avval

--- exercise/tamrin.py
+++ exercise/tamrin.py
@@ -22,3 +22,28 @@ print(result)
 # 2. magadire 3 va 5 ra be tartib daxele a va b garar dahid va bedune hafezeye ezafi jabeja konid
 # 3. magadire 3 va 5 va 7 ra be tartib daxele a va b va c garar dahid va jabeja konid
 # 4. magadire 3 va 5 va 7 ra be tartib daxele a va b va c garar dahid va bedune estefade az hafezeye ezafi jabeja konid.
+
+# 1.
+# a, b = 3, 5
+# c = a
+# a = b
+# b = c
+
+# 2.
+# a, b = 3, 5
+# a = a + b
+# b = a - b
+# a = a - b
+# a, b = 3, 5
+# a, b = b, a
+
+# 3.
+# a, b, c = 3, 5, 7
+# d = a
+# a = b
+# b = c
+# c = d
+
+# 4.
+# a, b, c = 3, 5, 7
+# a, b, c = b, c, a
